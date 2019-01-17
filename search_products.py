baa57bc867aa93e9c521371365840e57de90e555
Kyrin <agr68117@163.com>
2019-01-17 15:30:11 +0800

    增加 chrome 进程管理

--- search_products.py
+++ search_products.py
@@ -1,23 +1,27 @@
 # -*- coding:UTF-8 -*-
 
+import os, sys
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(base_dir + '/bin')
+driver_path = base_dir + '/bin/chromedriver'
 
 
-driver = webdriver.Chrome()
+# 初始化 chrome service
+chrome_service = Service(executable_path=driver_path)
+chrome_service.command_line_args()
+chrome_service.start()
+
+# 初始化 driver
+driver = webdriver.Chrome(driver_path)
 driver.implicitly_wait(30)
 driver.maximize_window()
 
 driver.get("http://www.linkedsee.com/")
 
-# search_field = driver.find_element_by_name('q')
-# search_field.clear()
-#
-# search_field.send_keys('phone')
-# search_field.submit()
-
-
 products = driver.find_elements_by_xpath("//ul[@class='drop box-1']/li/a")
-print(driver.find_element_by_css_selector("#xh-highlight").is_displayed())
 print(products)
 
 print("found " + str(len(products)) + "products")
@@ -26,3 +30,4 @@ for product in products:
     print(product.get_attribute('textContent'))
 
 driver.quit()
+chrome_service.stop()
